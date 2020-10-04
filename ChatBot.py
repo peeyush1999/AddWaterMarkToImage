e716242bf2b11db5d9d30158f45ce68c25fb2696
munnasahu1963 <57064135+munnasahu1963@users.noreply.github.com>
2020-10-04 15:05:03 +0530

    Create ChatBot.py

---
+++ ChatBot.py
@@ -0,0 +1,196 @@
+#Regular Expression based chat bot
+import string
+import re
+import random
+
+reflections = {
+  "am"     : "are",
+  "was"    : "were",
+  "i"      : "you",
+  "i'd"    : "you would",
+  "i've"   : "you have",
+  "i'll"   : "you will",
+  "my"     : "your",
+  "are"    : "am",
+  "you've" : "I have",
+  "you'll" : "I will",
+  "your"   : "my",
+  "yours"  : "mine",
+  "you"    : "me",
+  "me"     : "you"
+}
+
+
+class Chat(object):
+    def __init__(self, pairs, reflections={}):
+        """
+        Initialize the chatbot.  Pairs is a list of patterns and responses.  Each
+        pattern is a regular expression matching the user's statement or question,
+        e.g. r'I like (.*)'.  For each such pattern a list of possible responses
+        is given, e.g. ['Why do you like %1', 'Did you ever dislike %1'].  Material
+        which is matched by parenthesized sections of the patterns (e.g. .*) is mapped to
+        the numbered positions in the responses, e.g. %1.
+
+        @type pairs: C{list} of C{tuple}
+        @param pairs: The patterns and responses
+        @type reflections: C{dict}
+        @param reflections: A mapping between first and second person expressions
+        @rtype: C{None}
+        """
+
+        self._pairs = [(re.compile(x, re.IGNORECASE),y) for (x,y) in pairs]
+        self._reflections = reflections
+
+    # bug: only permits single word expressions to be mapped
+    def _substitute(self, str):
+        """
+        Substitute words in the string, according to the specified reflections,
+        e.g. "I'm" -> "you are"
+        
+        @type str: C{string}
+        @param str: The string to be mapped
+        @rtype: C{string}
+        """
+
+        words = ""
+        for word in string.split(string.lower(str)):
+            if self._reflections.has_key(word):
+                word = self._reflections[word]
+            words += ' ' + word
+        return words
+
+    def _wildcards(self, response, match):
+        pos = str.find(response,'%')
+        while pos >= 0:
+            num = string.atoi(response[pos+1:pos+2])
+            response = response[:pos] + \
+                self._substitute(match.group(num)) + \
+                response[pos+2:]
+            pos = string.find(response,'%')
+        return response
+
+    def respond(self, str):
+        """
+        Generate a response to the user input.
+        
+        @type str: C{string}
+        @param str: The string to be mapped
+        @rtype: C{string}
+        """
+
+        # check each pattern
+        for (pattern, response) in self._pairs:
+            match = pattern.match(str)
+
+            # did the pattern match?
+            if match:
+                resp = random.choice(response)    # pick a random response
+                resp = self._wildcards(resp, match) # process wildcards
+
+                # fix munged punctuation at the end
+                if resp[-2:] == '?.': resp = resp[:-2] + '.'
+                if resp[-2:] == '??': resp = resp[:-2] + '?'
+                return resp
+
+    # Hold a conversation with a chatbot
+    def converse(self,string, quit="quit"):
+        input1 = ""
+        while input1 != quit:
+            input1 = quit
+            #try: input1 = str(input(">"))
+            try: input1 = string
+            except EOFError:
+                return (input)
+            if input1:
+                while input1[-1] in "!.": input1 = input1[:-1]
+                return (self.respond(input1))
+
+
+
+
+                
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+     [
+        r"what is your name ?",
+        ["My name is Chatty and I'm a chatbot ?",]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing good\nHow about You ?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","Alright :)",]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program dude\nSeriously you are asking me this?",]
+        
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*) created ?",
+        ["Nagesh created me using Python's NLTK library ","top secret ;)",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Chennai, Tamil Nadu',]
+    ],
+    [
+        r"how is weather in (.*)?",
+        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
+    ],
+[
+        r"(.*)raining in (.*)",
+        ["No rain since last week here in %2","Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ",]
+    ],
+    [
+        r"(.*) (sports|game) ?",
+        ["I'm a very big fan of Football",]
+    ],
+    [
+        r"who (.*) sportsperson ?",
+        ["Messy","Ronaldo","Roony"]
+],
+    [
+        r"who (.*) (moviestar|actor)?",
+        ["Brad Pitt"]
+],
+    [
+        r"quit",
+        ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
+],
+]
+def chatty():
+        print("Hi, I'm Chatty and I chat alot ;)\nPlease type lowercase English language to start a conversation. Type quit to leave ") #default message at the start
+        chat = Chat(pairs, reflections)
+        msg = input('>')
+        while(msg != 'quit'):
+               print( chat.converse(string = msg))
+               msg = input('>')
+if __name__ == "__main__":
+    chatty()
